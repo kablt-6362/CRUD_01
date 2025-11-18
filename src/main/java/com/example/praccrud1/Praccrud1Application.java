6bdc1846e1baabb9f8da613f5fe9e74265fd4ac6
kablt--6362 <wogus6362@gmail.com>
2025-11-18 14:19:50 +0900

    project init

---
+++ src/main/java/com/example/praccrud1/Praccrud1Application.java
@@ -0,0 +1,13 @@
+package com.example.praccrud1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Praccrud1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Praccrud1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/praccrud1/Praccrud1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.praccrud1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Praccrud1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
